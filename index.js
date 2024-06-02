feacc4726c982888dd86556a508654790c2af0bf
kaleabmengesha <linuxkali717@gmail.com>
2024-06-02 05:38:07 -0700

    commit

--- index.js
+++ index.js
@@ -4,27 +4,40 @@
 let localdata = []
 
 
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
+const inputEl = document.getElementById("input")
+const inputBtn = document.getElementById("save")
+const deleteBtn = document.getElementById("clr")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("localdata") )
-const tabBtn = document.getElementById("tab-btn")
+
 
 if (leadsFromLocalStorage) {
     localdata = leadsFromLocalStorage
     render(localdata)
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        localdata.push(tabs[0].url)
-        localStorage.setItem("localdata", JSON.stringify(localdata) )
-        render(localdata)
+
+
+
+function main() {
+    
+    let tabBtn = document.getElementById("tab")
+    tabBtn.addEventListener("click", function(){    
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            localdata.push(tabs[0].url)
+            localStorage.setItem("localdata", JSON.stringify(localdata) )
+            render(localdata)
+        })
     })
-})
+}
+
+main()
+
+
 
 function render(leads) {
+
+const ulEl = document.getElementById("list")
+
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
